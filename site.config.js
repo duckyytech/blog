33b0010380e31c6a8517cf652bd9132bb51e8006
duckyytech <empanado_oculos.0y@icloud.com>
2023-10-20 18:25:34 +0100

    config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "duckyytech",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "quality engineer",
+    bio: "I test everything.",
+    email: "empanado_oculos.0y@icloud.com",
+    linkedin: "",
+    github: "duckyytech",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `blog`,
+      href: "https://github.com/duckyytech",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Simple is better than complex",
+    description: "welcome!",
   },
 
   // CONFIG configration (required)
@@ -47,9 +47,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "duckyytech/blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
